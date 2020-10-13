132a6dcfa6e3dddf3cae2c8311f998859303d4e2
prajwalmw <prajwalwaingankar@yahoo.in>
2020-10-13 10:07:49 +0530

    Data Binding is set up for Textview and Button.

--- app/src/main/java/com/prajwal/mvvmwithdatabinding/MainActivity.java
+++ app/src/main/java/com/prajwal/mvvmwithdatabinding/MainActivity.java
@@ -1,14 +1,35 @@
 package com.prajwal.mvvmwithdatabinding;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.prajwal.mvvmwithdatabinding.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+
+        //set text to textview
+        binding.buttonClick.setOnClickListener(view -> {
+            binding.textViewHello.setText("Prajwal has performed Data Binding");
+        });
+//        binding.buttonClick.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//
+//            }
+//        });
+
     }
+
 }
